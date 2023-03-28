45be4b67939b419715e58dbf7ba063e7705951b4
Rembqq <nikitavolkovskyi@gmail.com>
2023-03-28 12:26:26 +0300

    Descent completed

--- Sem2/Lab-1(2sem).c
+++ Sem2/Lab-1(2sem).c
@@ -1,34 +1,91 @@
 #include <stdio.h>
-int powr( int x, unsigned int n)
+#include <math.h>
+
+//we have to have index to pass the position between function calls
+float des_recurs_func(int i, float x, int n, float sum)
 {
-    int i = -1;
-    printf("i = %d n= %d \n", i , n);
-    if(n == 0){ i = 1; }
-    else
-    {
-        i = x*powr(x,n-1);
+    if (i == 1) {
+        sum++;
+        if (n == 1) {
+            return sum;
+        } else {
+            return des_recurs_func(i+1, x, n-1, sum);
+        }
+    } else if (i == 2) {
+        sum += -x/2;
+        if (n == 1) {
+            return sum;
+        } else {
+            return des_recurs_func(i+1, x, n-1, sum);
+        }
+    } else {
+        float f = des_recurs_func(i-1, x, n, 0) * x * (2*i-3)/(2*i);
+        sum += f;
+        if (n == 1) {
+            return sum;
+        } else {
+            return des_recurs_func(i+1, x, n-1, sum);
+        }
     }
-    printf("i = %d n= %d \n", i, n);
-    return i;
 }
-// }
-// int fib( unsigned int n) {
-//     int i = -1;
-//     if (n < 2) {
-//         i = n;
-//     }
-//     else
-//     {
-//         i = fib(n-2);
-//         i += fib(n-1);
-//     }
-//     return i;
-// }
+
+float comb_recurs_func(int n, float x)
+{
+    float sum = sqrt(1-x);
+    float fi = -1;
+    printf("fi = %f i = %d sum = %f\n", fi, n, sum);
+    if(n == 2)
+    {
+        fi = -x/2;
+    }
+    else    
+    {
+        fi = comb_recurs_func(n-1, x) * x * (2*n - 3) / (2 * n);
+        sum += fi;
+    }
+    printf("fi = %f i = %d sum = %f\n", fi, n, sum);  
+    return fi;
+}
+
+float ret_recurs_func(int n, float x)
+{
+    float sum = 0;
+    float fi = -1;
+    printf("fi = %f i = %d sum = %f\n", fi, n, sum);
+    
+    if(n == 2)
+    {
+        fi = -x/2;
+    }
+    else    
+    {
+        fi = ret_recurs_func(n-1, x) * x * (2*n - 3) / (2 * n);
+    }
+    sum = sqrt(1-x);
+    printf("fi = %f i = %d sum = %f\n", fi, n, sum);  
+    return fi;
+}
+
 int main()
 {
-    // int x, sum = 0;
-    // scanf("%d", &x);
-    // printf("%d", x);
-    powr(5, 4);
-    //fib(7);
+    int n, index = 1;
+    float x, sum = 0;
+    printf("Enter x(-1 < x < 1): ");
+    scanf("%f", &x);
+    printf("\n");
+    printf("Enter n(n > 2): ");
+    scanf("%d", &n);
+    
+    // Recursive Return
+    //printf("\nReturn\n");
+    //printf("%f",ret_recurs_func(n, x, 0));
+
+    // Descent
+    printf("\nRecursive Descent\n");
+    printf("%f",des_recurs_func(index, x, n, sum));
+
+    // // Combined
+    // printf("\nCombined\n");
+    // printf("%f",comb_recurs_func(index, x, n, sum));
+
 }
