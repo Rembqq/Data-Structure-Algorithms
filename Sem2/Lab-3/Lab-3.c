1b674140d998e0d76ff6b6d6c38d6f067c80b603
Rembqq <nikitavolkovskyi@gmail.com>
2023-04-26 16:17:35 +0300

    WinMain

---
+++ Sem2/Lab-3/Lab-3.c
@@ -0,0 +1,19 @@
+#include "windows.h"
+#include <math.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+char ProgName[]="Лабораторна робота 3";
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance)
+{
+
+}
+
+int WinMain()
+{
+    CreateWindow();
+    while (GetMessage)
+    {
+        
+    }
+    
+}
