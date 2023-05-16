fa6c50e638522360d655086f07d68d33074633bb
Rembqq <nikitavolkovskyi@gmail.com>
2023-05-17 00:20:36 +0300

    some changes

--- Sem2/Lab-3/Lab-3/Lab-3.c
+++ Sem2/Lab-3/Lab-3/Lab-3.c
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 char ProgName[] = "Лабораторна робота 3";
@@ -10,6 +12,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpszCmdLine, int nCmdShow)
 {
     WNDCLASS w;
+    
+    //unsigned float T[10][10];
+    /*for (int i = 0; i < 10; ++i)
+    {
+        for (int j = 0; j < 10; ++j)
+        {
+            T[i][j] = ;
+        }
+    }*/
+
     w.lpszClassName = ProgName;
     w.hInstance = hInstance;
     w.lpfnWndProc = WndProc;
@@ -31,8 +43,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
         WS_OVERLAPPEDWINDOW, //Стиль вікна - комплексний
         100, //положення верхнього кута вікна на екрані по х
         100, // положення верхнього кута вікна на екрані по y
-        460, //ширина вікна
-        240, //висота вікна
+        500, //ширина вікна
+        460, //висота вікна
         (HWND)NULL, // ідентифікатор породжуючого вікна
         (HMENU)NULL, //ідентифікатор меню (немає)
         (HINSTANCE)hInstance, //ідентифікатор екземпляра вікна
@@ -84,21 +96,43 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT messg,
     switch (messg) {
     case WM_PAINT:
         hdc = BeginPaint(hWnd, &ps);
-        char* nn[3] = { "1", "2", "3" };
-        int nx[3] = { 100,200,300 };
-        int ny[3] = { 100,100,100 };
+        char* nn[10] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", L"10"};
+        int nx[10] = { 125,155,200,275,320, 350, 320, 275, 200, 155};
+        int ny[10] = { 200,140,90 ,90 ,140, 200, 260, 310, 310, 260};
         int dx = 16, dy = 16, dtx = 5;
         int i;
+
+        float ugu;
+        srand(2041);
+        bool adjacency_matrix[10][10];
+        for (int i = 0; i < 10; ++i)
+        {
+            for (int j = 0; j < 10; ++j)
+            {
+                ugu = (float)rand() / (float)RAND_MAX));
+                adjacency_matrix[i][j] = (((1.0 - 0 * 0.02 - 1 * 0.005 - 0.25) * (ugu)
+                     * 2) >= 1.0) ? true : false;
+                if (adjacency_matrix[i][j])
+                {
+                    MoveToEx(hdc, nx[i], ny[i], NULL);
+                    LineTo(hdc, nx[j], ny[j]);
+                    /*arrow(hdc, 0, nx[1] - dx, ny[1]);
+                    Arc(hdc, nx[0], ny[0] - 40, nx[2], ny[2] + 40, nx[2], ny[2], nx[0] - 40, ny[0] - 40);
+                    arrow(hdc, -45.0, nx[2] - dx * 0.5, ny[2] - dy * 0.8);*/
+                }
+            }
+        }
         HPEN BPen = CreatePen(PS_SOLID, 2, RGB(50, 0, 255));
         HPEN KPen = CreatePen(PS_SOLID, 1, RGB(20, 20, 5));
         SelectObject(hdc, KPen);
-        MoveToEx(hdc, nx[0], ny[0], NULL);
+
+       /* MoveToEx(hdc, nx[0], ny[0], NULL);
         LineTo(hdc, nx[1], ny[1]);
         arrow(hdc, 0, nx[1] - dx, ny[1]);
-        Arc(hdc, nx[0], ny[0] - 40, nx[2], ny[2] + 40, nx[2], ny[2], nx[0], ny[0]);
-        arrow(hdc, -45.0, nx[2] - dx * 0.5, ny[2] - dy * 0.8);
+        Arc(hdc, nx[0], ny[0] - 40, nx[2], ny[2] + 40, nx[2], ny[2], nx[0]-40, ny[0]-40);
+        arrow(hdc, -45.0, nx[2] - dx * 0.5, ny[2] - dy * 0.8);*/
         SelectObject(hdc, BPen);
-        for (i = 0; i <= 2; i++) {
+        for (i = 0; i <= 9; i++) {
             Ellipse(hdc, nx[i] - dx, ny[i] - dy, nx[i] + dx, ny[i] + dy);
             TextOut(hdc, nx[i] - dtx, ny[i] - dy / 2, nn[i], 1);
         }
